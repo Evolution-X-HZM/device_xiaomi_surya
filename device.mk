a95f0bf8f56754c2d85ecaa0f0778f4b94d7bcb8
Hazama25 <hazamafawkes@gmail.com>
2023-08-09 06:55:35 +0000

    surya: Evolution-X bringup
    " LET'S GO OFFICIAL "
    
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -323,9 +323,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_surya/com.nxp.mifare.xml
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
 PRODUCT_PACKAGES += \
--- evolution_surya.mk
+++ evolution_surya.mk
@@ -8,16 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from surya device
+# Inherit from surya device.
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := OFFICIAL
 
-PRODUCT_NAME := lineage_surya
+# Device identifier.
+PRODUCT_NAME := evolution_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
